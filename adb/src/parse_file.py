80f6d8ac138e3c8da8af35e4c281d121b8a9d6d4
Liheng.Gong <liheng.gong@hotmail.com>
2018-12-03 23:19:44 -0500

    update README

--- adb/src/parse_file.py
+++ adb/src/parse_file.py
@@ -1,4 +1,5 @@
-from transaction import start_cur_trans
+import re
+import transaction
 
 
 def read_parse_file(filename):
@@ -7,23 +8,51 @@ def read_parse_file(filename):
             parse_line(line.strip())
 
 
+def find_transaction_number(line):
+    lst_res = re.findall(r'T\d+', line)
+    return int(lst_res[0][1:]) if len(lst_res) > 0 else None
+
+
+def find_variable_ind(line):
+    lst_res = re.findall(r'x\d+', line)
+    return int(lst_res[0][1:]) if len(lst_res) > 0 else None
+
+
+def find_variable_val(line):
+    lst_res = line.split(',')
+    return int(lst_res[2].strip()) if len(lst_res) > 2 else None
+
+
 def parse_line(line):
     if line.startswith('begin('):
-        start_cur_trans('typeRW', 0)
+        trans_num = find_transaction_number(line)
+        print('begin trans num', trans_num)
+        transaction.start_cur_trans('typeRW', 0)
     elif line.startswith('beginRO('):
-        start_cur_trans('typeRO', 0)
+        transaction.start_cur_trans('typeRO', 0)
     elif line.startswith('W('):
-        print('transaction w')
+        v_ind = find_variable_ind(line)
+        v_val = find_variable_val(line[2:-1])
+        print('write variable ind', v_ind, 'variable val', v_val)
+        transaction.write_operation()
     elif line.startswith('R('):
-        print('transaction r')
+        trans_num = find_transaction_number(line)
+        print('read trans num', trans_num)
+        transaction.read_operation()
     elif line.startswith('end('):
-        print('end trans')
+        trans_num = find_transaction_number(line)
+        print('end trans num', trans_num)
+        transaction.end_transaction()
     elif line.startswith('recover('):
-        print('recover')
+        site_number = find_variable_val(line)
+        print('recover', site_number)
+        transaction.recover_site()
     elif line.startswith('fail('):
-        print('fail')
+        site_number = find_variable_val(line)
+        print('fail', site_number)
+        transaction.fail_site()
     elif line.startswith('dump('):
-        print('dump')
+        transaction.dump_info()
 
 
 if __name__ == '__main__':
